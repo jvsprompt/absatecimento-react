7788cf5ddccf7ebc23d993f0af22e46fa3f82b5e
fprompto <fernando.monteiro.f11@gmail.com>
2022-07-26 11:29:21 -0300

    Alteração na rota da página do formulário

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
     <AppProvider>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Form />} />
+          <Route path="/formulario" element={<Form />} />
           <Route path="/equipamentos" element={<Equipaments />} />
         </Routes>
       </BrowserRouter>
