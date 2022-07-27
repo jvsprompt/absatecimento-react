a776b05d1e93b5e985b86f8131bc07ec6a03de14
fprompto <fernando.monteiro.f11@gmail.com>
2022-07-27 11:46:19 -0300

    Search adicionado

--- src/pages/Equipaments.jsx
+++ src/pages/Equipaments.jsx
@@ -7,20 +7,65 @@ import EquipamentsFile from '../data/equipaments.json';
 
 function Equipaments() {
   const [equips, setEquips] = useState([]);
+  const [filteredEquips, setFilteredEquips] = useState([]);
+  const [search, setSearch] = useState('');
   const [isLoading, setIsLoading] = useState(true);
+
+  const updateSearch = () => {
+    const searchNormalize = search
+      .normalize('NFD')
+      .replace(/[\u0300-\u036f]/g, "")
+      .toLowerCase();
+    
+    const filter = equips.filter(({ name, equipamento, setor, tag }) => {
+      const nameNormalize = name
+        .normalize('NFD')
+        .replace(/[\u0300-\u036f]/g, "")
+        .toLowerCase();
+      const equipamentoNormalize = equipamento
+        .normalize('NFD')
+        .replace(/[\u0300-\u036f]/g, "")
+        .toLowerCase();
+      const setorNormalize = setor
+        .normalize('NFD')
+        .replace(/[\u0300-\u036f]/g, "")
+        .toLowerCase();
+      const tagNormalize = tag
+        .normalize('NFD')
+        .replace(/[\u0300-\u036f]/g, "")
+        .toLowerCase();
+
+      return nameNormalize.includes(searchNormalize) || equipamentoNormalize.includes(searchNormalize) || setorNormalize.includes(searchNormalize) || tagNormalize.includes(searchNormalize);
+    });
+
+    setFilteredEquips(filter);
+  };
+
+  useEffect(() => {
+    updateSearch()
+  }, [search]);
 
   useEffect(() => {
     const loadEquips = async () => {
-      setEquips(EquipamentsFile)
+      setEquips(EquipamentsFile);
+      setFilteredEquips(EquipamentsFile);
       setIsLoading(false);
+      
     }
     loadEquips();
   }, [])
 
   return isLoading ? <Loading/> : (
     <div className=''>
+      <input
+        type="text"
+        className='form-control nav-items search-bar'
+        placeholder='Pesquisar'
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
       <div className='equip-list'>
-        {equips.map((eq) => {
+        {filteredEquips.map((eq) => {
           return (
             <EquipCard
               id={eq.id}
