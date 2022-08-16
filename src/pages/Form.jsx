f19e0ec83a833ef68f14c454bdf6db7d1fff7cdb
Jean <jeanvicentvicente@gmail.com>
2022-08-16 08:55:06 -0300

    Correção dos Bugs de Setor Errado

--- src/pages/Form.jsx
+++ src/pages/Form.jsx
@@ -10,25 +10,19 @@ import setor from '../data/setores.json';
 import servico from '../data/servicos.json';
 
 function Form() {
-  // const [encarregadoValue, setEncarregadoValue] = useState('');
   const [servicosValue, setServicosValue] = useState(servico[0]);
   const [dataLevantValue, setDataLevantValue] = useState(new Date());
-  // const [data1ExValue, setData1ExValue] = useState('');
-  // const [data2ExValue, setData2ExValue] = useState('');
   const [unidadesValue, setUnidadesValue] = useState(unidades[0]);
   const [setorValue, setSetorValue] = useState('');
-  // const [osValue, setOsValue] = useState('');
   const [materiaisValue, setMateriaisValue] = useState('');
 
   const entry = {
-    // ecarregado: 'entry.848539894',
     servico: 'entry.1136451657',
     date: 'entry.1309448216',
     unidade: 'entry.275485717',
     setor: 'entry.1073286246',
     materiais: 'entry.1986892611',
     os: 'entry.1053084165',
-
     day: 'entry.1309448216_day',
     month: 'entry.1309448216_month',
     year: 'entry.1309448216_year',
@@ -114,10 +108,6 @@ function Form() {
     alert(testData);
   };
 
-  // useEffect(() => {
-  //   setSetorValue(setor1);
-  // }, []);
-
   useEffect(() => {
     setSetorValue(setor1);
   }, [unidadesValue]);
@@ -133,13 +123,6 @@ function Form() {
 
   return (
     <div className='main-div'>
-      {/* <InputDropdown
-        name='ENCARREGADO'
-        value={encarregadoValue}
-        change={setEncarregadoValue}
-        items={['ENCARREGADO 1', 'ENCARREGADO 2', 'ENCARREGADO 3']}
-        localStore={true}
-      /> */}
       <InputDropdown
         name='SERVIÇO'
         value={servicosValue}
@@ -155,18 +138,6 @@ function Form() {
         localStore={false}
       // classN='input2 '
       />
-      {/* <InputDate
-        name='1ª DATA DE EXECUÇÃO'
-        value={data1ExValue}
-        change={setData1ExValue}
-        localStore={false}
-      />
-      <InputDate
-        name='2ª DATA DE EXECUÇÃO'
-        value={data2ExValue}
-        change={setData2ExValue}
-        localStore={false}
-      /> */}
       <InputDropdown
         name='UNIDADE'
         value={unidadesValue}
@@ -181,12 +152,6 @@ function Form() {
         items={getSetor(unidadesValue)}
         localStore={false}
       />
-      {/* <InputText
-        name='OS'
-        value={osValue}
-        change={setOsValue}
-        localStore={false}
-      /> */}
       <InputTextArea
         name='MATERIAIS'
         value={materiaisValue}
@@ -195,8 +160,6 @@ function Form() {
         cols='30'
         rows='6'
         maxLen={1500}
-      // minLen={9}
-      // classN='input2 '
       />
       <button className="myButton" type="submit" onClick={sendData}>Enviar</button>
       <div className='botton'></div>
