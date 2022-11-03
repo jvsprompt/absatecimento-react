2f7b2e449a8c2d613c3a47c90515052d24130da8
Jean <jeanvicetvicente@gmail.com>
2022-11-03 16:56:58 -0300

    Pasta SRC-Cópia deletada

--- src - Copia/utils/normalizeString.js
+++
@@ -1,8 +0,0 @@
-const normalizeString = (str) => {
-  return str
-    .normalize('NFD')
-    .replace(/[\u0300-\u036f]/g, "")
-    .toLowerCase();
-};
-
-export default normalizeString;
--- src - Copia/utils/removeSpaces.js
+++
@@ -1,7 +0,0 @@
-function removeSpaces(str) {
-  const newStr = str.replace(/\s/g, '');
-  return newStr.normalize('NFD')
-    .replace(/[\u0300-\u036f]/g, "").toLowerCase();
-}
-
-export default removeSpaces;
