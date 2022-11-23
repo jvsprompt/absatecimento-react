1467d0ecc58f772b44ebe45445971cbba685726b
Jean <jeanvicetvicente@gmail.com>
2022-11-23 16:57:18 -0300

    alterar HMON_URI e ENTRY_HMON

--- src/config/strings.js
+++ src/config/strings.js
@@ -1,6 +1,6 @@
 export const ACADEMIA_URI = 'https://docs.google.com/forms/u/0/d/e/1FAIpQLSdEkBoaxNY5D8JJqKmBwcpcJHVfhJmMvigYJIsvAN5FdKmCoQ/formResponse';
 
-export const HMON_URI = '';
+export const HMON_URI = 'https://docs.google.com/forms/u/2/d/e/1FAIpQLSeaG7ME235aq00cLAxyOVW4LnekoTI68chxGB7bkbuiduRYtQ/formResponse';
 
 export const ENTRY_ACADEMIA = {
   servico: 'entry.72998713',
@@ -20,18 +20,11 @@ export const ENTRY_ACADEMIA = {
 };
 
 export const ENTRY_HMON = {
-  servico: 'entry.72998713',
-  date: 'entry.1309448216',
-  unidade: 'entry.275485717',
+  cliente: 'entry.72998713',
+  codigo: 'entry.1309448216',
+  material: 'entry.275485717',
   setor: 'entry.1073286246',
-  materiais: 'entry.1986892611',
-  os: 'entry.1053084165',
-  day: 'entry.1309448216_day',
-  month: 'entry.1309448216_month',
-  year: 'entry.1309448216_year',
-  day2: 'entry.72998713_day',
-  month2: 'entry.72998713_month',
-  year2: 'entry.72998713_year',
-  enc: 'entry.848539894',
-  serv: 'entry.1136451657'
+  qtd: 'entry.1986892611',
+  unid: 'entry.1053084165',
+  obs: 'entry.1309448216_day',
 };
